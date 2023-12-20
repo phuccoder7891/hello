74e0671a690c0b1616ff8561aa261fb9fd0901c4
phuccoder7891 <151824608+phuccoder7891@users.noreply.github.com>
2023-12-20 11:34:51 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const API_URL = "https://api.openai.com/v1/chat/completions";
+const strIndex = "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";
+const system_message = "You are a smart robot!"; // You can change this message
+const path = "/lehoangphucdeptrai";  // Change path to your path. For localhost make it path = "";
+// const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
